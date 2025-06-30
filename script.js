81d65d8be077bcfaafec4c8e5392d61b13c1ae88
Sin-drael <floriancourtalhac1@gmail.com>
2025-06-30 10:09:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Exemple simple de code JavaScript
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Le site est entièrement chargé !');
+
+    // Vous pouvez ajouter des interactions ici, par exemple :
+    // const titrePrincipal = document.querySelector('h1');
+    // titrePrincipal.addEventListener('click', () => {
+    //     alert('Vous avez cliqué sur le titre !');
+    // });
+});
